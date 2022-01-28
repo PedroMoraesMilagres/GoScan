7d45efaf33db92e7bc2f59444633b2d47694c52d
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2022-01-28 19:38:16 -0300

    Switch println - fmt.Println

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
-import("flag"; "os"; "os/exec"; "github.com/ArthurHydr/GoScan/port")
+import("fmt"; "flag"; "os"; "os/exec"; "github.com/ArthurHydr/GoScan/port")
 
 func menu(){
-  println("  .d8888b.          .d8888b.                         ") 
-  println(" d88P  Y88b        d88P  Y88b                        ")
-  println(" 888    888        Y88b.                              ")
-  println(" 888        .d88b.  \"Y888b.   .d8888b 8888b. 88888b.  ")
-  println(" 888  88888d88\"\"88b    \"Y88b.d88P\"       \"88b888 \"88b ")
-  println(" 888    888888  888      \"888888     .d888888888  888 ")
-  println(" Y88b  d88PY88..88PY88b  d88PY88b.   888  888888  888 ")
-  println("  \"Y8888P88 \"Y88P\"  \"Y8888P\"  \"Y8888P\"Y888888888  888 ")
-  println("\nGoScan v1.0\n")
+  fmt.Println("  .d8888b.          .d8888b.                         ") 
+  fmt.Println(" d88P  Y88b        d88P  Y88b                        ")
+  fmt.Println(" 888    888        Y88b.                              ")
+  fmt.Println(" 888        .d88b.  \"Y888b.   .d8888b 8888b. 88888b.  ")
+  fmt.Println(" 888  88888d88\"\"88b    \"Y88b.d88P\"       \"88b888 \"88b ")
+  fmt.Println(" 888    888888  888      \"888888     .d888888888  888 ")
+  fmt.Println(" Y88b  d88PY88..88PY88b  d88PY88b.   888  888888  888 ")
+  fmt.Println("  \"Y8888P88 \"Y88P\"  \"Y8888P\"  \"Y8888P\"Y888888888  888 ")
+  fmt.Println("\nGoScan v1.0\n")
 }
 
 
