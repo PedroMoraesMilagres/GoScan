c1d2ada9cbdd7b6d4f110cf9294471be1b1a6dd7
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2022-01-29 03:28:27 -0300

    Fix tabs

--- port/port.go
+++ port/port.go
@@ -1,32 +1,32 @@
 package port
 
 import (
-	"fmt"
-	"net"
-	"strconv"
-	"sync"
-	"time"
+  "fmt"
+  "net"
+  "strconv"
+  "sync"
+  "time"
 )
 
 type PortResult struct {
-	Port    int
-	State   bool
-	Service string
+  Port    int
+  State   bool
+  Service string
 }
 
 type PortRange struct {
-	Start, End int
+  Start, End int
 }
 
 type ScanResult struct {
-	hostname string
-	ip       []net.IP
-	results  []PortResult
+  hostname string
+  ip       []net.IP
+  results  []PortResult
 }
 
 var common = map[int]string{
   7:    "echo",
- 	19:   "chargen",
+  19:   "chargen",
   20:   "ftp",
   21:   "ftp",
   22:   "ssh",
@@ -64,67 +64,67 @@ var common = map[int]string{
 }
 
 func ScanPort(protocol, hostname, service string, port int, resultChannel chan PortResult, wg *sync.WaitGroup) {
-	defer wg.Done()
-	result := PortResult{Port: port, Service: service}
-	address := hostname + ":" + strconv.Itoa(port)
-	conn, err := net.DialTimeout(protocol, address, 1*time.Second)
+  defer wg.Done()
+  result := PortResult{Port: port, Service: service}
+  address := hostname + ":" + strconv.Itoa(port)
+  conn, err := net.DialTimeout(protocol, address, 1*time.Second)
 	if err != nil {
-		result.State = false
-		resultChannel <- result
-		return
-	}
-	defer conn.Close()
-	result.State = true
-	resultChannel <- result
+    result.State = false
+    resultChannel <- result
+    return
+  }
+  defer conn.Close()
+  result.State = true
+  resultChannel <- result
 }
 
 func ScanPorts(hostname string, ports PortRange) (ScanResult, error) {
-	var results []PortResult
-	var scanned ScanResult
-	var wg sync.WaitGroup
+  var results []PortResult
+  var scanned ScanResult
+  var wg sync.WaitGroup
 
-	resultChannel := make(chan PortResult, ports.End-ports.Start)
+  resultChannel := make(chan PortResult, ports.End-ports.Start)
 
-	addr, err := net.LookupIP(hostname)
-	if err != nil {
-		return scanned, err
-	}
-	for i := ports.Start; i <= ports.End; i++ {
-		if service, ok := common[i]; ok {
-			wg.Add(1)
-			go ScanPort("tcp", hostname, service, i, resultChannel, &wg)
+  addr, err := net.LookupIP(hostname)
+  if err != nil {
+    return scanned, err
+  }
+  for i := ports.Start; i <= ports.End; i++ {
+    if service, ok := common[i]; ok {
+      wg.Add(1)
+      go ScanPort("tcp", hostname, service, i, resultChannel, &wg)
 
-		}
-	}
-	wg.Wait()
-	close(resultChannel)
-	for result := range resultChannel {
-		results = append(results, result)
-	}
+  }
+  }
+  wg.Wait()
+  close(resultChannel)
+  for result := range resultChannel {
+    results = append(results, result)
+  }
 
-	scanned = ScanResult{
-		hostname: hostname,
-		ip:       addr,
-		results:  results,
-	}
-	return scanned, nil
+  scanned = ScanResult{
+    hostname: hostname,
+    ip:       addr,
+    results:  results,
+  }
+  return scanned, nil
 }
 
 func DisplayResult(result ScanResult) {
-	ip := result.ip[len(result.ip)-1]
+  ip := result.ip[len(result.ip)-1]
   fmt.Printf("Hostname: %s | IP: %s\nPORT | SERVICE\n", result.hostname, ip.String())
-	for _, v := range result.results {
-		if v.State {
-			fmt.Printf("%d	%s\n", v.Port, v.Service)
-		}
-	}
+  for _, v := range result.results {
+    if v.State {
+      fmt.Printf("%d	%s\n", v.Port, v.Service)
+    }
+  }
 }
 
 func GetPorts(hostname string, ports PortRange) {
-	scanned, err := ScanPorts(hostname, ports)
-	if err != nil {
+  scanned, err := ScanPorts(hostname, ports)
+  if err != nil {
     fmt.Println("Misuse! Correct way to use: ./main.go -host <address>")
-	} else {
-		DisplayResult(scanned)
-	}
+  } else {
+    DisplayResult(scanned)
+  }
 }
